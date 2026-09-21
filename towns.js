f53c68ce3dd25eda8c4105a378a21385a0b482fb
IvelinGyaurov <massa9424@gmail.com>
2026-09-21 16:36:42 +0300

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
+	$('#btnAdd').click(addTown);
 });
+
+
 
 function deleteTown() {
 	let townName = $('#townName').val();
@@ -17,3 +20,11 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+function addTown() {
+	let townName = $('#townNameForAdd').val();
+	$('#townNameForAdd').val('');
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
+}
+
